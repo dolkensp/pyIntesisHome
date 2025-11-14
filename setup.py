333ae65d3a30f051e2dcdee85d9aae1f430c82ef
Peter Dolkens <github@ddrit.com>
2025-11-14 17:28:33 +1100

    Bump version from 1.8.8 to 1.8.9

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pyintesishome2",
-    version="1.8.8",
+    version="1.8.9",
     description="A python3 library for running asynchronus communications with IntesisHome Smart AC Controllers",
     long_description=long_description,
     long_description_content_type="text/markdown",
